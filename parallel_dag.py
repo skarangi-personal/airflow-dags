f5b3c62dfc73e038fa62dc45cfc94ab320fdd291
admin_workstation_cloud9 <ec2-user@ip-172-31-9-119.us-east-2.compute.internal>
2020-12-25 18:10:21 +0000

    changed the dag start date

--- parallel_dag.py
+++ parallel_dag.py
@@ -5,7 +5,7 @@ from airflow.operators.python_operator import PythonOperator
 from datetime import datetime
 
 default_args = {
-    'start_date': datetime(2019, 1, 1),
+    'start_date': datetime(2020, 12, 24),
     'owner': 'Airflow',
 }
 
